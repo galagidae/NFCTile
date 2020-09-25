dbc0336cb96e012f08744f6f9f29b0459b9c41f0
Anthony Benbrook <anthonybenbrook@hotmail.com>
2020-09-25 19:20:21 +0100

    Add tile

---
+++ NFCTile/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Service.QuickSettings;
+using Android.Views;
+using Android.Widget;
+
+namespace NFCTile
+{
+    [Service(Name = "com.galagidae.NFCTile",
+             Permission = Android.Manifest.Permission.BindQuickSettingsTile,
+             Label = "NFC",
+             Icon = "@drawable/ic_nfc_tile")]
+    [IntentFilter(new[] { ActionQsTile })]
+    class Tile : TileService
+    {
+        public override void OnTileAdded()
+        {
+            base.OnTileAdded();
+        }
+
+        public override void OnStartListening()
+        {
+            base.OnStartListening();
+        }
+
+        public override void OnStopListening()
+        {
+            base.OnStopListening();
+        }
+
+        public override void OnTileRemoved()
+        {
+            base.OnTileRemoved();
+        }
+    }
+}
